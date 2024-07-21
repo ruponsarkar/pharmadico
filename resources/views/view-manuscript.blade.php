b5a422d87d338821c8e2c3e3c884816f20def455
mriganka Hazarika <hmrigank@gmail.com>
2024-07-21 15:16:39 +0530

    manu view

--- resources/views/view-manuscript.blade.php
+++ resources/views/view-manuscript.blade.php
@@ -5,11 +5,60 @@
 <!-- this is journal page -->
 
 @section('content')
+
+<style>
+    thead {
+        background-color: pink;
+        color : #fff
+    }
+</style>
+
 <div class="container">
     <div class="row journal-list">
-       <div class="col-8">
+        <div class="col-8">
+            <div>
+                <h4>ManuScript Details</h4>
+            </div>
+            <table class="table">
+                <thead>
 
-       </div>
+                    <tr>
+                        <td scope="row">Manuscript No</td>
+                        <td>84djsndj</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scope="row">Manuscript Title</td>
+                        <td>backend Manuscript Title</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td scope="row">Authors Name</td>
+                        <td>mriganka hazarika</td>
+                    </tr>
+                </tbody>
+            </table>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <td scope="row">Date </td>
+                        <td>Status</td>
+                        <td>Corresponsing Author</td>
+                        <td>Journal Name</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scope="row">Manuscript Title</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">Authors Name</td>
+                        <td>mriganka hazarika</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 
