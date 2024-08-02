657d22ac77c23b228e794baff52cdf675a92ffa9
Rupon Sarkar <ruponsarkar108@gmail.com>
2024-08-02 18:33:11 +0530

    final

---
+++ resources/views/mails/acknowledge.blade.php
@@ -0,0 +1,42 @@
+<html>
+
+<body>
+
+
+
+    Dear {{$name}},
+    <br />
+    We have received your Article for publication in {{$journal}} Coming Issue,
+    <br />
+    Your Manuscript No. {{$muuid}},
+    <br />
+    <br />
+
+    Use this reference no. for further communication,
+    <br />
+
+    Also you can check your article status on,
+    <br />
+
+    https://pharmedicopublishers.com
+    <br />
+    <br />
+
+    Soon we will inform you of your status in your article.
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    --
+    <br />
+    Editor in Chief
+    <br />
+    International Journal of Pharmaceutical Science and Medicine
+    <br />
+    https://pharmedicopublishers.com/
+    <br />
+
+</body>
+
+</html>
---
+++ resources/views/mails/admin.blade.php
@@ -0,0 +1,31 @@
+<html>
+    <body>
+    Hii admin,
+    <br/>
+    we have received one Manuscript.
+    <br/>
+    kindly find the details on your admin panel
+
+    <br/>
+    <br/>
+    Author Name: {{$name}}
+    <br/>
+    Author Email: {{$email}}
+    <br/>
+    Phone : {{$phone}}
+    <br/>
+    Manuscript Title : {{$title}}
+    <br/>
+    Article Type: {{$type}} 
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    Thank you
+    <br/>
+    Admin
+
+
+    </body>
+</html>
